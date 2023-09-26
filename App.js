b3c205c574b9837d7afae512cb68339fe6fdb57b
huỳnh trân <20130444@st.hcmuaf.edu.vn>
2023-09-26 18:39:14 +0700

    up code Customers.js Control_Bill.js Control_Company.js

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Profile from './screen/Profile';
 import Home from './screen/Home';
+import Product from './screen/Products';
+import Bill from './screen/Control_Bill';
+import Customer from './screen/Customers';
+import Company from './screen/Control_Company';
 
 export default function App() {
-    return <Home />;
+    return <Company />;
 }
 
 const styles = StyleSheet.create({});
